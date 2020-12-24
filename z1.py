40e2f8091c5c6a1cf4da03cdea72fcffc3ab4c0a
Max Vishevski <visyaskj@gmail.com>
2020-12-24 19:15:07 +0300

    z1.py

---
+++ z1.py
@@ -0,0 +1,21 @@
+# 1. Даны 2 де ствительных числа a и b. Получить их сумму, разность и произведение.
+
+a = 5
+
+print(a)
+
+b = 7
+
+print(b)
+
+sum = a + b
+
+print(sum)
+
+difference = a - b
+
+print(difference)
+
+composition = a * b
+
+print(composition)
